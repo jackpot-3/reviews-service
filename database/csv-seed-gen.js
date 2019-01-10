af2dd8203e4c305e166484d9e70b292ef0532947
Bill Murphy <bill559m@gmail.com>
2019-01-10 15:33:01 -0800

    csv setup for chunking data to file in chuncks

--- database/csv-seed-gen.js
+++ database/csv-seed-gen.js
@@ -2,37 +2,41 @@ console.log('csv seed generator');
 const faker = require('faker');
 const fs = require('fs');
 
-const maxRecords = 1000;
-const maxReviews = 5000;
-// const connection = require('./db.js');
-console.time('Start CSV Generation');
-// const createProductQuery = 'INSERT INTO products (ID) VALUES (?)';
+const usbMount = `/media/bill/3FE0-6909/`;
+
+const maxRecords = 10000000;
+const maxReviews = 10;
+
+const maxChunckSize = 10000;
+
+const t0 = new Date().getTime();
+
+let fakeDataRec = '';
+let fakeDataRev = '';
 
 for (let i = 1; i <=  maxRecords; i += 1) {
   const current = i;
 
-  // console.log('record: ' + current);
-  
-  let fakeData = current + '\r\n';
-  // console.log('fakeData: ' + fakeData);
+  const fakeDataRecord = `${current}\r\n`;
 
-  try {
-    fs.appendFileSync('records.txt', fakeData);
-    // console.log('The "data to append" was appended to file!');
-  } catch (err) {
-    console.log('Error writing csv records chunk to file') /* Handle the error */
+  fakeDataRec += fakeDataRecord;
+
+  if (i % maxChunckSize === 0) {
+    try {
+      fs.appendFileSync(`${usbMount}records.txt`, fakeDataRec);
+      // console.log('The "data to append" was appended to file!');
+    } catch (err) {
+      console.log('Error writing csv records chunk to file'); /* Handle the error */
+    }
+
+    fakeDataRec = '';
   }
-  // connection.query(createProductQuery, [current], (err) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  // });
 }
 
 // const createReviewQuery = 'INSERT INTO reviews (product_id, username, is_verified, review_text, score, found_helpful, title, review_date) VALUES (?,?,?,?,?,?,?,?)';
 
-for (let i = 1; i <=  maxRecords; i++) {
-  // Generate 0 to 10 reviews per product 
+for (let i = 1; i <= maxRecords; i++) {
+  // Generate 0 to 10 reviews per product
   const numberOfReviews = maxReviews; // temp replace
   for (let j = 0; j < numberOfReviews; j++) {
     const productId = i;
@@ -43,28 +47,27 @@ for (let i = 1; i <=  maxRecords; i++) {
     const title = faker.lorem.words(3);
     
     const date = faker.date.between('2010-01-01', '2018-12-1');
-    let fakeData = [productId, username, 1, reviewText, score, foundHelpful, title, date];
-    fakeData.join(',');
-    fakeData = fakeData + '\r\n';
+    let fakeDataReviews = [productId, username, 1, reviewText, score, foundHelpful, title, date];
+    fakeDataReviews.join(',');
+    fakeDataReviews = `${fakeDataReviews}\r\n`;
 
     // console.log('fakeData: ', fakeData);
     
-    // console.log('Product no: ' + productId + ' Review no: ' + j);
-    
-    try {
-      fs.appendFileSync('reviews.txt', fakeData);
-      // console.log('The "data to append" was appended to file!');
-    } catch (err) {
-      console.log('Error writing csv reviews chunk to file')/* Handle the error */
+    fakeDataRev += fakeDataReviews;
+
+    if (j % maxChunckSize === 0) {
+      try {
+        fs.appendFileSync(`${usbMount}reviews.txt`, fakeDataRev);
+        // console.log('The "data to append" was appended to file!');
+      } catch (err) {
+        console.log('Error writing csv reviews chunk to file')/* Handle the error */
+      }
+
+      fakeDataRev = '';
     }
-    // connection.query(createReviewQuery, fakeData, (err) => {
-    //   if (err) {
-    //     // eslint-disable-next-line no-console
-    //     console.log(err);
-    //   }
-    // });
   }
 }
 
-console.timeEnd('End CSV Generation');
-
+const t1 = new Date().getTime();
+console.log(`To write ${maxRecords * maxReviews} records the `);
+console.log(`Elapsed time: ${((t1 - t0) / 1000)} seconds!`);
